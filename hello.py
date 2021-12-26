cf8213eae3564a6fff0b76ab4e115a24014525ab
Kyungwoon Na <nakw0410@gmail.com>
2021-12-26 17:45:20 +0900

    feat: Python 웹스크래핑(크롤링)

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
-import requests # requests 라이브러리 설치 필요
+import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-rjson = r.json()
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
-gus = rjson['RealtimeCityAir']['row']
+soup = BeautifulSoup(data.text, 'html.parser')
 
-for gu in gus:
-    gu_name = gu['MSRSTE_NM']
-    gu_mise = gu['IDEX_MVL']
+trs = soup.select('#old_content > table > tbody > tr')
 
-    if(gu_mise > 50):
-        print(gu_name, gu_mise)
+for tr in trs:
+    a_tag = tr.select_one('td.title > div > a')
+    if a_tag is not None:
+        title = a_tag.text
+        print(title)
